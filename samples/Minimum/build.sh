64d0ca51bf9643e44743cf9f49e73d4f91ee9cb6
Yohsuke Murase <yohsuke.murase@gmail.com>
2016-09-02 18:01:43 +0900

    add Minimum sample project

---
+++ samples/Minimum/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -eux
+
+SCRIPTDIR=$(cd $(dirname $BASH_SOURCE); pwd)
+BUILD=${BUILD:-build}
+mkdir -p ${BUILD}
+BUILD=$(cd $BUILD && pwd) # get absolute path
+IS_MPI=${IS_MPI:-0}
+
+if [ $IS_MPI = 1 ]; then
+  x10c++ -v -O -x10rt mpi -sourcepath ${SCRIPTDIR}/../..:${SCRIPTDIR} -d ${BUILD} ${SCRIPTDIR}/Main.x10 -VERBOSE_CHECKS
+else
+  x10c++ -v -O            -sourcepath ${SCRIPTDIR}/../..:${SCRIPTDIR} -d ${BUILD} ${SCRIPTDIR}/Main.x10 -VERBOSE_CHECKS
+fi
+
