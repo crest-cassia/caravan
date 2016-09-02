145b976cbaafede76defddf4f96db545f1d654c6
Yohsuke Murase <yohsuke.murase@gmail.com>
2016-09-02 17:30:24 +0900

    refactored sample projects

--- samples/SASample/caravan/main.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef MAIN_HPP
-#define MAIN_HPP
-
-double RunSimulator( double x1, double x2, double x3 );
-
-#endif
--- samples/WeightedSocialNetwork/simulator/random.cpp
+++
@@ -1,3 +0,0 @@
-#include "random.hpp"
-
-std::vector<boost::random::mt19937> Random::m_rnds;
