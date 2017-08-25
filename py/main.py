7bef8afa3172ee7de8d4751507eefba3e9ac1bf0
Yohsuke Murase <yohsuke.murase@gmail.com>
2017-08-25 17:11:57 +0900

    initial commit of search_engine

--- py/main.py
+++ py/main.py
@@ -15,26 +15,49 @@ def load_setting(config_file):
 max_submitted_run_id = 0
 
 def _submit(run):
+    global max_submitted_run_id
     s_point = " ".join( [str(x) for x in run.parameter_set().point] )
     out = "%d %s %d\n" % (run.id, s_point, run.seed)
     sys.stdout.write(out)
+    max_submitted_run_id = len(tables.runs_table)
 
 def submit_new_runs():
+    global max_submitted_run_id
     un_submitted = tables.runs_table[max_submitted_run_id:]
     for r in un_submitted:
         if not r.is_finished():
             _submit(r)
+
+def _parse_line(line):
+    l = line.split(' ')
+    rid,rc,place_id,start_at,finish_at = [ int(x) for x in l[:5] ]
+    results = [ float(x) for x in l[5:] ]
+    r = tables.runs_table[rid]
+    r.store_result(results,place_id,start_at,finish_at)
+    return r
+
+def receive_finished_run():
+    line = sys.stdin.readline()
+    line = line.rstrip()
+    if not line:
+        return None
+    return _parse_line(line)
+
 
 load_setting('config.json')
 
 se.create_initial_runs()
 submit_new_runs()
 
-#line = sys.stdin.readline()
-#while line:
-#    finished_ps = parse_line(line)
-#    if finished_ps:
-#        on_parameter_set_finished(finished_ps)
-#        submit_new_runs()
-#    line = sys.stdin.readline()
+while True:
+    r = receive_finished_run()
+    if r:
+        ps = r.parameter_set()
+        if ps.is_finished():
+            se.on_parameter_set_finished(ps)
+            submit_new_runs()
+    else:
+        break
+
+tables.dump('table.dmp')
 
--- py/search_engine.py
+++ py/search_engine.py
@@ -1,7 +1,6 @@
 import setting
 import tables
 import parameter_set
-import run
 
 
 def create_initial_runs():
@@ -10,4 +9,8 @@ def create_initial_runs():
         ps = parameter_set.ParameterSet.find_or_create(p)
         ps.create_runs_upto(3)
 
+def on_parameter_set_finished(finished_ps):
+    if finished_ps.point == (0,1,2):
+        ps = parameter_set.ParameterSet.find_or_create((6,7,8))
+        ps.create_runs_upto(3)
 
