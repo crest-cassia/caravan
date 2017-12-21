34b292c55f38649d65335e305e1f30bd43feb721
Yohsuke Murase <yohsuke.murase@gmail.com>
2017-12-22 08:49:39 +0900

    added script to run benchmark on K

--- samples/benchmark/run_bench_k.sh
+++ samples/benchmark/run_bench_k.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 #============ pjsub Options ============
-#PJM --rsc-list "node=1032"
+#PJM --rsc-list "node=2304"
 #PJM --rsc-list "elapse=00:10:00"
 #PJM --rsc-list "rscgrp=large"
-#PJM --mpi "proc=8256"
+#PJM --mpi "proc=18432"
 #PJM --stg-transfiles all
 #PJM --mpi "use-rankdir"
-#PJM --stgin "rank=* ../../build/a.out %r:./"
-#PJM --stgin "rank=0 ./bench_problem3.py %r:./"
+#PJM --stgin  "rank=* ../../build/a.out %r:./"
+#PJM --stgin  "rank=0 ./bench_problem.py %r:./"
 #PJM --stgin "rank=0 ../../python_module.tar %r:./"
 #PJM --stgout-dir "rank=0 %r:./ %j"
 #PJM --stgout "rank=0 %r:./stderr.txt.%r ./%j/"
@@ -28,7 +28,7 @@ export X10RT_MPI_THREAD_SERIALIZED=1
 
 tar xf python_module.tar
 export PYTHONPATH=python_module:$PYTHONPATH
-export CARAVAN_LOG_LEVEL=${CARAVAN_LOG_LEVEL:-2}
+export CARAVAN_LOG_LEVEL=${CARAVAN_LOG_LEVEL:-1}
 
-mpiexec  -ofout-proc stdout.txt -oferr-proc stderr.txt ./a.out python -u ./bench_problem3.py 165120 2 1.0 20.0
+mpiexec -ofout-proc stdout.txt -oferr-proc stderr.txt ./a.out python -u ./bench_problem.py 184320 0 0.25 4 20.0 8.0
 
