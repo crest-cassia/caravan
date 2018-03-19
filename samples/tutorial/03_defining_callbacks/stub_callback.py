11175129ed0291a4b0c75307c96f81feeb05c155
Yohsuke Murase <yohsuke.murase@gmail.com>
2018-03-19 16:44:28 +0900

    implemented stub codes

--- samples/tutorial/03_defining_callbacks/stub_callback.py
+++ samples/tutorial/03_defining_callbacks/stub_callback.py
@@ -13,5 +13,3 @@ with start_stub(stub_sim, num_proc = 16):
         task = Task.create("sleep %d" % (i%3+1))
         task.add_callback(lambda t, i=i: Task.create("sleep %d" % (i%3+1)))
 
-Task.dump_binary('tasks.bin')
-
