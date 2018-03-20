9c9d971f5fc9ba8d5aec06229b4f4b15ebc6cf87
Yohsuke Murase <yohsuke.murase@gmail.com>
2018-03-20 13:58:03 +0900

    bugfix of samples

--- samples/tutorial/04_async_await/hello_await_all.py
+++ samples/tutorial/04_async_await/hello_await_all.py
@@ -5,6 +5,6 @@ from caravan.task import Task
 with Server.start():
     tasks = [ Task.create( "sleep %d" % (t%3+1) ) for t in range(5) ]
     Server.await_all_tasks( tasks )                   # this method blocks until all the tasks are finished
-    print("all running tasks finished", file=sys.stderr)
+    print("all running tasks finished. Adding more tasks", file=sys.stderr, flush=True)
     tasks = [ Task.create( "sleep %d" % (t%3+1) ) for t in range(5) ]
 
