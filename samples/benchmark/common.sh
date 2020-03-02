c1712976f26de5a986d55fdf11c43fd41ee2531d
Yohsuke Murase <yohsuke.murase@gmail.com>
2020-03-02 13:33:18 +0900

    adopting new scheduler

--- samples/benchmark/common.sh
+++ samples/benchmark/common.sh
@@ -2,20 +2,15 @@
 
 SCRIPTDIR=$(cd $(dirname $BASH_SOURCE); pwd)
 
-export X10_NPLACES=${X10_NPLACES:-8}
+export N_PROCS=8
 CARAVAN_DIR=$SCRIPTDIR/../..
-SCHEDULER=$CARAVAN_DIR/caravan_scheduler/scheduler
+SCHEDULER=${HOME}/sandbox/caravan_cpp/cmake-build-debug/caravan_scheduler
 export PYTHONPATH=$CARAVAN_DIR/caravan_search_engine:$PYTHONPATH
+
 export CARAVAN_SEND_RESULT_INTERVAL=${CARAVAN_SEND_RESULT_INTERVAL:-0}
 export export CARAVAN_SEARCH_ENGINE_LOGLEVEL=${CARAVAN_SEARCH_ENGINE_LOGLEVEL:-INFO}
 export CARAVAN_LOG_LEVEL=${CARAVAN_LOG_LEVEL:-1}
 
 CMD="${SCHEDULER} ${SE}"
-
-IS_MPI=${IS_MPI:-0}
-if [ $IS_MPI = 1 ]; then
-  mpiexec -n $X10_NPLACES $CMD
-else
-  $CMD
-fi
+mpiexec -np ${N_PROCS} ${CMD}
 
