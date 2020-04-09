c0ca05eb868091092ab70cecf7c73c74f9782ffe
Yohsuke Murase <yohsuke.murase@gmail.com>
2020-04-09 13:42:38 +0900

    maintenance of test_optimization_samples

--- test/test_optimization_samples.py
+++ test/test_optimization_samples.py
@@ -3,10 +3,6 @@ import tempfile
 import subprocess
 
 
-def eprint(*s):
-    print(*s, file=sys.stderr, flush=True)
-
-
 class OptimizationSamplesTest(unittest.TestCase):
     def setUp(self):
         self.caravan_dir = os.path.abspath(os.path.dirname(__file__) + "/..")
@@ -15,8 +11,7 @@ class OptimizationSamplesTest(unittest.TestCase):
         script = self.caravan_dir + "/samples/optimization/run_opt.sh"
         with tempfile.TemporaryDirectory() as tmpdir:
             subprocess.run([script], check=True, cwd=tmpdir, timeout=30)
-            self.assertTrue(os.path.exists(tmpdir + "/tasks.bin"))
-            # assert output of the optimization
+            self.assertTrue(os.path.exists(tmpdir + "/tasks.msgpack"))
             self.assertTrue(os.path.exists(tmpdir + "/opt_log.txt"))
             with open(tmpdir + "/opt_log.txt") as f:
                 best_f = float(f.readlines()[-1].split()[-2])
