592be1ac230c850cbe569fc87e4dd052f0d1b7f4
Yohsuke Murase <yohsuke.murase@gmail.com>
2020-04-29 12:39:50 +0900

    disable file I/O

--- caravan_scheduler/Task.hpp
+++ caravan_scheduler/Task.hpp
@@ -41,17 +41,17 @@ class Task {
   std::string command;
   json input;
   TaskResult Run(Logger& logger, const std::chrono::system_clock::time_point& ref_time, const std::string& work_base_dir, long timeout) const {
-    const fs::path cwd = fs::current_path();
+    // const fs::path cwd = fs::current_path();
 
     const fs::path work_dir = WorkDirPath(work_base_dir);
 
-    fs::create_directories(work_dir);
-    if(!input.is_null()) {
-      std::ofstream fout(InputFilePath(work_base_dir).string());
-      fout << input;
-      fout.flush();  // explicitly flush fout
-    }
-    fs::current_path(work_dir);
+    // fs::create_directories(work_dir);
+    // if(!input.is_null()) {
+    //   std::ofstream fout(InputFilePath(work_base_dir).string());
+    //   fout << input;
+    //   fout.flush();  // explicitly flush fout
+    // }
+    // fs::current_path(work_dir);
 
     auto start_at = std::chrono::system_clock::now();
     long s_at = std::chrono::duration_cast<std::chrono::milliseconds>(start_at - ref_time).count();
@@ -66,23 +66,23 @@ class Task {
     auto finish_at = std::chrono::system_clock::now();
     long f_at = std::chrono::duration_cast<std::chrono::milliseconds>(finish_at - ref_time).count();
     logger.d("Completed task %d", task_id);
-    fs::current_path(cwd);
+    // fs::current_path(cwd);
 
     int my_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
     TaskResult res(task_id, rc, my_rank, s_at, f_at);
 
-    const fs::path res_path = OutputFilePath(work_base_dir);
-    logger.d("Output file %s", res_path.c_str());
-    if( fs::exists(res_path) ) {
-      size_t res_size = fs::file_size(res_path);
-      logger.d("Output file is found for task %d (%d bytes)", task_id, res_size);
-      json j;
-      std::ifstream fin(res_path.string());
-      fin >> j;
-      res.output = j;
-      logger.d("Output: %s", res.output.dump().c_str() );
-    }
+    // const fs::path res_path = OutputFilePath(work_base_dir);
+    // logger.d("Output file %s", res_path.c_str());
+    // if( fs::exists(res_path) ) {
+    //   size_t res_size = fs::file_size(res_path);
+    //   logger.d("Output file is found for task %d (%d bytes)", task_id, res_size);
+    //   json j;
+    //   std::ifstream fin(res_path.string());
+    //   fin >> j;
+    //   res.output = j;
+    //   logger.d("Output: %s", res.output.dump().c_str() );
+    // }
 
     return res;
   }
