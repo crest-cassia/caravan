58ddd216a766b393a6d4ff651377c051e2a3cefc
Yohsuke Murase <yohsuke.murase@gmail.com>
2020-06-11 07:48:36 +0900

    maintenance

--- samples/benchmark/run_bench1.sh
+++ samples/benchmark/run_bench1.sh
@@ -4,7 +4,6 @@ set -x
 
 export CARAVAN_SEARCH_ENGINE_LOGLEVEL=DEBUG
 HERE=$(cd $(dirname $BASH_SOURCE); pwd)
-SE="$HERE/bench_problem.py 2 1 0.25 4 2.0 0.5"
+SE="python3 $HERE/bench_problem.py 2 1 0.25 4 2.0 0.5"
 
 source ${HERE}/common.sh
-
