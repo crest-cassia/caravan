0a8e606e31d2d36a9379bdc00fafe55cf9144da6
Yohsuke Murase <yohsuke.murase@gmail.com>
2020-07-20 21:37:44 +0900

    bugfix

--- caravan_search_engine/caravan/__init__.py
+++ caravan_search_engine/caravan/__init__.py
@@ -2,9 +2,7 @@ import sys
 if 'unittest' not in sys.modules:
     # when unit-testing, skip importing Server as it depends on mpi4py
     from .server import Server
-else:
-    print('unittest module is detected. skip importing Server')
+    from .stub_server import StubServer
 from .task import Task
-from .stub_server import StubServer
 from .simulator import Simulator
 from .tables import Tables
